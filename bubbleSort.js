8629b3409c0e6c48aaf6d8bbbcaaebbfec5d467a
LucasPerezDev <LucasPerezDev@gmail.com>
2022-10-27 23:59:58 -0400

    Added an explanation of the logic for this approach of sorting.

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,9 +1,15 @@
-// Write a function that takes in an array of integers and returns a sorted version of that array. 
+// Write a function that takes in an array of integers and 
+//returns a sorted version of that array. 
 // Use bubble sort algorithm to sort the array.
 
 // Sample input: array = [8, 5, 2, 9, 5, 6, 3]
 // Sample output: [2, 3, 5, 5, 6, 8, 9]
 
+//Bubble Sort is the simplest sorting algorithm that works by 
+//looping through the input array and  swapping the adjacent 
+//elements if they are in the wrong order. 
+//This algorithm is not suitable for large data sets as its 
+//average and worst-case time complexity is quite high.
 
 //Worst: O(n^2) time | O(1) space complexity
 
